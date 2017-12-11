509102391823016caeb665ddee4d0111bd126c92
MildlySpicy <fattyfatfatyogurt@gmail.com>
2017-12-10 21:15:03 -0500

    I forgot what i changed

--- BeepBoop.py
+++ BeepBoop.py
@@ -2,17 +2,20 @@
 
 import praw
 
-class Bot: 
-  bot = praw.Reddit(user_agent='KindaFeedingBot',
-                    client_id='p_Y2_d0xk1Cm-Q',
-                    client_secret='8q469EsTHNUyRxg2LSUYRVlLiOc',
-                    username='ShittySkynetBot',
-                    password='J19900101t'
-                    )
 
-  subreddit = bot.subreddit('uoft')
-  multireddit = bot.subreddit('dankmemes+memeeconomy+meirl+me_irl+deepfriedmemes+classic4chan+bikinibottomtwitter+bertstrips+anime_irl+4chan+2meirl4meirl+prequelmemes')
+class Bot:
+  """Its a bot, wow"""
 
-
-  comments = subreddit.stream.comments()
-  multicomments = multireddit.stream.comments()
+  subreddits = None
+  comments = None
+  
+  def __init__(self, subs: str):
+    bot = praw.Reddit(user_agent = 'KindaFeedingBot',
+                      client_id = 'p_Y2_d0xk1Cm-Q',
+                      client_secret = '8q469EsTHNUyRxg2LSUYRVlLiOc',
+                      username = 'ShittySkynetBot',
+                      password = 'J19900101t'
+                      )
+  
+    self.subreddits = bot.subreddit(subs)
+    self.comments = self.subreddits.stream.comments()
